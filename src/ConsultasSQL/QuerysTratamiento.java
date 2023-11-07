4a5b1d1d16ba5e79cb2bc70d3e3c23bdb4a140ff
Arturo <132175372+HarolArturoFonsecaMendoza@users.noreply.github.com>
2023-11-06 22:54:44 -0600

    Harol Arturo Mendoza
    Tercer Sprint

---
+++ src/ConsultasSQL/QuerysTratamiento.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ConsultasSQL;
+
+
+public class QuerysTratamiento { 
+    private int id;
+    private String nombre;
+    private String tipoTratamiento;
+    private String Descripcion;
+    private byte[] Foto1;
+    private byte[] Foto2;
+    private String NomreProductos;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipoTratamiento() {
+        return tipoTratamiento;
+    }
+
+    public void setTipoTratamiento(String tipoTratamiento) {
+        this.tipoTratamiento = tipoTratamiento;
+    }
+    
+    public byte[] getFoto1() {
+        return Foto1;
+    }
+
+    public void setFoto1(byte[] Foto1) {
+        this.Foto1 = Foto1;
+    }
+
+    public byte[] getFoto2() {
+        return Foto2;
+    }
+
+    public void setFoto2(byte[] Foto2) {
+        this.Foto2 = Foto2;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+    
+       public String getNomreProductos() {
+        return NomreProductos;
+    }
+
+    public void setNomreProductos(String NomreProductos) {
+        this.NomreProductos = NomreProductos;
+    }
+    
+    public static String RegistroTratamiento = "INSERT INTO tratamientos("
+            +"Nombre,"+"tipoTratamiento,"+"Descripcion,"+"Foto1,"+"Foto2,"+"NombreProductos)"
+            +"VALUES(?,?,?,?,?,?)";
+    
+    public static String ListarTratamiento = "SELECT * FROM tratamientos";
+    
+    public static String VerTratamiento = "SELECT Nombre, tipoTratamiento,Descripcion, Foto1, Foto2, NombreProductos FROM tratamientos WHERE id = ?";
+    public static String VerFoto = "SELECT Foto1, Foto2 FROM tratamientos WHERE id = ?";
+    
+}

3b06fc18a7d0aa512c0a728b7415c9ab6a2ac59d
CristhianAvila1994 <93703616+CristhianAvila1994@users.noreply.github.com>
2023-11-15 11:04:29 -0600

    nueva

---
+++ src/ConsultasSQL/QuerysDevoluciones.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ConsultasSQL;
+
+public class QuerysDevoluciones {
+
+    public static Object getItemAt(int selectedIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    private int id;
+    private String id_ventas;
+    private String producto_devolver;
+    private String descipcion;
+    private String fecha;
+    private int precio_devolver;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getId_ventas() {
+        return id_ventas;
+    }
+
+    public void setId_ventas(String id_ventas) {
+        this.id_ventas = id_ventas;
+    }
+
+    public String getProducto_devolver() {
+        return producto_devolver;
+    }
+
+    public void setProducto_devolver(String producto_devolver) {
+        this.producto_devolver = producto_devolver;
+    }
+    
+
+    public String getDescipcion() {
+        return descipcion;
+    }
+
+    public void setDescipcion(String descipcion) {
+        this.descipcion = descipcion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+   
+    public int getPrecio_devolver() {
+        return precio_devolver;
+    }
+
+    public void setPrecio_devolver(int precio_devolver) {
+        this.precio_devolver = precio_devolver;
+    }
+    
+    
+}
